35f88900a2aaa9e0608b86af936b28f47cd3ea8f
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-02-25 13:38:22 +0100

    form validation fonction done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // CONSIGNES EXERCICE : 
 // Le formulaire sera envoyé grâce à la méthode "Post". ------------------------ OK
-// La "Société" doit comporter au moins 1 caractère. ---------------------------
-// La "Personne à contacter" doit comporter au moins 1 caractère. --------------
-// Le "Code postal" doit comporter 5 caractères numériques. --------------------
+// La "Société" doit comporter au moins 1 caractère. --------------------------- OK
+// La "Personne à contacter" doit comporter au moins 1 caractère. -------------- OK 
+// Le "Code postal" doit comporter 5 caractères numériques. -------------------- OK 
 // La "Ville" doit comporter au moins 1 caractère. -----------------------------
 // Le Email doit comporter au moins le caractère "@". --------------------------
 
@@ -34,12 +34,50 @@ const emailErr = document.getElementById("emailErr");
 // --- DECLARATION DES FONCTIONS
 
 form.addEventListener('submit', (e) => {
-    let errorMessage;
-    if (societe.value.length < 1) {
-
+    //Message d'erreur si les valeurs sont fausses. 
+    if (societe.value.length <= 1) {
+        societeErr.innerHTML = "Le nom de la société doit être renseigné";
         e.preventDefault()
-    }
+    };
 
-    //taking controll of the submit btn 
-    e.preventDefault()
+    if (personneAContacter.value.length <= 1) {
+        personneAContacterErr.innerHTML = "Le nom de la personne à contacter doit être renseigné";
+        e.preventDefault()
+    };
+
+    if (isNaN(codePostal.value) || codePostal.value.length != 5) {
+        codePostalErr.innerHTML = "Votre code postal doit être composé de 5 chiffres";
+        e.preventDefault()
+    };
+
+    if (ville.value.length <= 1) {
+        villeErr.innerHTML = "Le nom de la ville doit être renseigné";
+        e.preventDefault()
+    };
+
+    if (!email.value.includes("@")) {
+        emailErr.innerHTML = "Votre email doit contenir un @";
+        e.preventDefault()
+    };
+
+    //Retrait du message d'erreur si les valeurs sont bonnes. 
+    if (societe.value.length >= 1) {
+        societeErr.innerHTML = "";
+    };
+
+    if (personneAContacter.value.length >= 1) {
+        personneAContacterErr.innerHTML = "";
+    };
+
+    if (!isNaN(codePostal.value) && codePostal.value.length == 5) {
+        codePostalErr.innerHTML = "";
+    };
+
+    if (ville.value.length >= 1) {
+        villeErr.innerHTML = "";
+    };
+
+    if (email.value.includes("@")) {
+        emailErr.innerHTML = "";
+    };
 })
